cf116cad4dc1e60162bf2dcfbb79e8a245b0dcfb
mathkarl <mathkarl@LSE-B2725>
2020-01-16 11:18:21 +0000

    d

---
+++ TestGeneric/ListOfObjects.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGeneric
+{
+    class ListOfObjects<T>
+    {
+        private T[] theList;
+        private int noOfElements = 10;
+        private int nextIndex;
+
+        public ListOfObjects()
+        {
+            theList = new T[noOfElements];
+            nextIndex = 0;
+        }
+
+        public void AddObject(T o)
+        {
+            theList[nextIndex] = o;
+            nextIndex += 1;
+        }
+        public T GetObject(int index)
+        {
+            return theList[index];
+        }
+    }
+}
--- TestGeneric/Program.cs
+++ TestGeneric/Program.cs
@@ -22,6 +22,22 @@ namespace TestGeneric
             String middle = DataSample<string>.getMiddleElement(words);
 
             Console.WriteLine(middle);
+
+            ListOfObjects<string> list = new ListOfObjects<string>();
+            list.AddObject("Forty-two");
+            list.AddObject("Twenty-one");
+            list.AddObject("Three");
+            list.AddObject("Sixty-six");
+            list.AddObject("Twenty");
+            list.AddObject("Ten");
+            list.AddObject("Fifty-five");
+            list.AddObject("Twenty-three");
+            list.AddObject("Twelve");
+            list.AddObject("Eleven");
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine(list.GetObject(index));
+            }
             Console.ReadKey();
         }
     }
